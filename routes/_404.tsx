92c8fc02d4ebb10593afa5265620bc79f0723912
sugawani <nekorollykk@gmail.com>
2023-04-19 01:43:40 +0900

    🚀 404 page - Auto redirect index after 5 minutes

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,9 +1,14 @@
 import { UnknownPageProps } from "$fresh/server.ts";
+import { Head } from "$fresh/src/runtime/head.ts";
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
   return (
     <div>
+      <Head>
+        <meta http-equiv="refresh" content="5;url=/" />
+      </Head>
       <p>Woops! {url.pathname} is not found... 😢</p>
+      <p>Redirect index after 5minutes...🐑</p>
     </div>
   );
 }
